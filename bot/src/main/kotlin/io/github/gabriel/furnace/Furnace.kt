9348760e152512899e028c6e8830c889be5e73dd
Gabriel <gabriel@lunala.xyz>
2022-04-14 13:37:44 -0300

    chore: oops, forgot to remove that

--- bot/src/main/kotlin/io/github/gabriel/furnace/Furnace.kt
+++ bot/src/main/kotlin/io/github/gabriel/furnace/Furnace.kt
@@ -6,7 +6,7 @@ private lateinit var _furnace: FurnaceBot
 val furnace: FurnaceBot get() = _furnace
 
 suspend fun main() {
-    _furnace = FurnaceBot(DeckClient("MRRQZABSsU6gYdXn6mDT5pSNsSBFkUoYD4l6YZ3PccPhJLjoK+8fVNirRzEpQEBxJ5ria+VAtZ5hobPeXDKBIQ=="))
+    _furnace = FurnaceBot(DeckClient(""))
     furnace.start()
 }
 
